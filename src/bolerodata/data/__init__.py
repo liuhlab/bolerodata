a03390c450270dfca65b39bcb0705116c092a773
Hanqing Liu <hanqingliu@fas.harvard.edu>
2025-03-26 14:38:25 -0700

    add sample metadata

--- src/bolerodata/data/__init__.py
+++ src/bolerodata/data/__init__.py
@@ -2,7 +2,8 @@ import pandas as pd
 import pathlib
 
 STANDARD_CELL_METADATA_DIR = pathlib.Path("/large_storage/zhoulab/hanliu/wmb/standard/cell_metadata")
-
+STANDARD_SAMPLE_METADATA_DIR = pathlib.Path("/large_storage/zhoulab/hanliu/wmb/standard/sample_metadata")
+STANDARD_CLUSTER_METADATA_DIR = pathlib.Path("/large_storage/zhoulab/hanliu/wmb/standard/cluster_metadata")
 
 class Metadata:
     def __init__(self):
--- src/bolerodata/dataset.py
+++ src/bolerodata/dataset.py
@@ -1,5 +1,10 @@
 import pandas as pd
-from .data import metadata, STANDARD_CELL_METADATA_DIR
+from .data import (
+    metadata,
+    STANDARD_CELL_METADATA_DIR,
+    STANDARD_CLUSTER_METADATA_DIR,
+    STANDARD_SAMPLE_METADATA_DIR,
+)
 
 
 class Dataset:
@@ -15,8 +20,6 @@ class Dataset:
         self.technology = self.metadata["technology"]
         self.genome = self.metadata["genome"]
 
-        self.cell_metadata_path = self.metadata["cell_metadata"]
-        self.sample_metadata_path = self.metadata["sample_metadata"]
         self.peak_bed_path = self.metadata["peaks"]
         self.peak_adata_path = self.metadata["peak_adata"]
         self.gene_adata_path = self.metadata["gene_adata"]
@@ -44,10 +47,43 @@ class Dataset:
         return self._cache["standard_cell_metadata"]
 
     @property
+    def sample_metadata(self) -> pd.DataFrame:
+        """
+        Standard sample metadata table.
+
+        Schema:
+        - Index: sample_id
+            - This id match the sample column in all cell_metadata.
+            - This correspond to technical sample (e.g., one 10X experiments)
+              the same barcode within sample should come from a single cell.
+            - For biological sample, record in donor column
+        - Columns:
+            1. age  # time, standard age group
+            2. age_int  # Estimated postconceptional age in days, standard age group int
+            3. tissue  # space, experiment tissue source information
+            4. DissectionRegion  # space, anatomical descriptive vocabulary of the tissue
+            5. sex  # exp design
+            6. donor  # exp design
+        - Additional Columns:
+            - **age_details
+            - **tissue_details
+            - **exp_details
+        """
+        if "sample_metadata" not in self._cache:
+            self._cache["sample_metadata"] = pd.read_feather(
+                STANDARD_SAMPLE_METADATA_DIR / f"{self.name}.sample_metadata.feather"
+            )
+        return self._cache["sample_metadata"]
+
+    @property
     def original_cell_metadata(self):
-        """Cell metadata table organized from original study, contain misc information."""
+        """
+        Cell metadata table organized from original study, contain misc information.
+        """
         if "cell_metadata" not in self._cache:
-            self._cache["cell_metadata"] = pd.read_feather(self.cell_metadata_path)
+            self._cache["cell_metadata"] = pd.read_feather(
+                self.metadata["cell_metadata"]
+            )
         return self._cache["cell_metadata"]
 
 
@@ -77,7 +113,8 @@ class Datasets:
         return name in self.available_datasets
 
     def __iter__(self):
-        for dataset in self.available_datasets:
+        for name in self.available_datasets:
+            dataset = Dataset(name)
             yield dataset
 
 
