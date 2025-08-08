9727a39baeea267e447a26c8b78299392b43709e
Hanqing Liu <hanqingliu@fas.harvard.edu>
2025-08-08 08:59:44 -0700

    add more embedding benchmark related paths

--- src/bolerodata/data/_path_patch.py
+++ src/bolerodata/data/_path_patch.py
@@ -4,35 +4,36 @@ EmbeddingBenchmarkDir = (
     "/large_storage/zhoulab/hanliu/250728-EmbeddingBenchmark/metacell_dataset"
 )
 
-human_brain_dev_paths = {}
-human_brain_dev_parquet_paths = {}
-for emb_type in ["PVI", "PANVI", "SCVI", "SCANVI", "STATE"]:
-    _d = {
-        (
-            "cell",
-            "HumanBrainDev",
-            emb_type,
-        ): f"{EmbeddingBenchmarkDir}/adata/metadata_and_coords/HumanBrainDev+{emb_type}.cell.h5ad",
-        (
-            "metadata",
-            "HumanBrainDev",
-            emb_type,
-        ): f"{EmbeddingBenchmarkDir}/adata/metadata_and_coords/HumanBrainDev+{emb_type}.metacell.h5ad",
-        (
-            "peak",
-            "HumanBrainDev",
-            emb_type,
-        ): f"{EmbeddingBenchmarkDir}/adata/peak/HumanBrainDev+{emb_type}.meta_cell.peak_count.h5ad",
-    }
-    human_brain_dev_paths.update(_d)
-    _d = {
-        (
-            "32bp",
-            "HumanBrainDev",
-            emb_type,
-        ): f"{EmbeddingBenchmarkDir}/parquet/HumanBrainDev+{emb_type}/HumanBrainDev-MetaCell-32bp",
-    }
-    human_brain_dev_parquet_paths.update(_d)
+embedding_benchmark_paths = {}
+embedding_benchmark_parquet_paths = {}
+for dataset in ["HumanBrainDev", "AIBSDev"]:
+    for emb_type in ["PVI", "PANVI", "SCVI", "SCANVI", "STATE"]:
+        _d = {
+            (
+                "cell",
+                dataset,
+                emb_type,
+            ): f"{EmbeddingBenchmarkDir}/adata/metadata_and_coords/{dataset}+{emb_type}.cell.h5ad",
+            (
+                "metadata",
+                dataset,
+                emb_type,
+            ): f"{EmbeddingBenchmarkDir}/adata/metadata_and_coords/{dataset}+{emb_type}.metacell.h5ad",
+            (
+                "peak",
+                dataset,
+                emb_type,
+            ): f"{EmbeddingBenchmarkDir}/adata/peak/{dataset}+{emb_type}.meta_cell.peak_count.h5ad",
+        }
+        embedding_benchmark_paths.update(_d)
+        _d = {
+            (
+                "32bp",
+                dataset,
+                emb_type,
+            ): f"{EmbeddingBenchmarkDir}/parquet/{dataset}+{emb_type}/{dataset}-MetaCell-32bp",
+        }
+        embedding_benchmark_parquet_paths.update(_d)
 
 PATH_PATCH_DICT = {
     "META_CELL_ADATA_PATH_DICT": {
@@ -66,7 +67,7 @@ PATH_PATCH_DICT = {
             "Zu2023Nature",
             "CTXGlutPVI",
         ): f"{EmbeddingBenchmarkDir}/adata/peak/Zu2023Nature+CTXGlutPVI.meta_cell.peak_count.h5ad",
-        **human_brain_dev_paths,
+        **embedding_benchmark_paths,
     },
     "META_CELL_PARQUET_PATH_DICT": {
         (
@@ -84,6 +85,6 @@ PATH_PATCH_DICT = {
             "Zu2023Nature",
             "CTXGlutPVI",
         ): f"{EmbeddingBenchmarkDir}/parquet/Zu2023Nature+CTXGlutPVI/Zu2023Nature-MetaCell-32bp",
-        **human_brain_dev_parquet_paths,
+        **embedding_benchmark_parquet_paths,
     },
 }
